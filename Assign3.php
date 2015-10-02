c228fb9f5a48b33800c837f4c6916ba4c45c6336
Patrick Viker <pviker@me.com>
2015-10-02 08:51:53 -0500

    CSS update

--- Assign3.php
+++ Assign3.php
@@ -7,6 +7,7 @@
 	<title>Assignment Three - MySQL</title>
 	<meta charset = "utf-8">
 	<link rel="stylesheet" type="text/css" href="styles/queryresults.css">
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
 	
 </head>
 
@@ -36,53 +37,50 @@
 	        </form>
 	    
 	     </div>
-	     
+	     <div>
 	     <?php
 	     
-	    if(isset($_POST['btn_submit'])) {
+	     	date_default_timezone_set('America/Chicago');
 	     
-	     @ $db = mysqli_connect('localhost', 'user1', 'abc123', 'events');
-	
-	if(mysqli_connect_errno() ) {
-		
-		echo "Error: could not connect to database. Please try again later.";
-		exit;
-	}
-		
-	$query = "select selected, category, month, day, name from events where month = '" . $_POST['btn_submit'] ."'";
-	$result = mysqli_query($db, $query);
-	
-					echo "<table id='center'>
-	     	
-	     	
-	     	<tr>
-	     		<th>Today's date is: " . date('l jS \of F Y') . "</th>
-	     	</tr>
-	     	
-	     	<tr>
-	     		<th class='colheaders'>Selected</th>
-	     		<th class='colheaders'>Category</th>
-	     		<th class='colheaders''>Month</th>
-	     		<th class='colheaders'>Day</th>
-	     		<th class='colheaders'>Name</th>
-	     	</tr>";
+			if(isset($_POST['btn_submit'])) {
+			     
+			@ $db = mysqli_connect('localhost', 'user1', 'abc123', 'events');
 			
-					 while($row = mysqli_fetch_assoc($result)) {
+			if(mysqli_connect_errno() ) {
+				echo "Error: could not connect to database. Please try again later.";
+				exit;
+			}
+				
+			$query = "select selected, category, month, day, name from events where month = '" . $_POST['btn_submit'] ."'";
+			$result = mysqli_query($db, $query);
+			
+				echo "<table id='center'>
+			    		<tr>
+			     			<th colspan=\"5\">Today's date is: " . date('l jS \of F Y') . "</th>
+			     		</tr>
+			     	
+				     	<tr>
+				     		<th class='colheaders'>Selected</th>
+				     		<th class='colheaders'>Category</th>
+				     		<th class='colheaders''>Month</th>
+				     		<th class='colheaders'>Day</th>
+				     		<th class='colheaders'>Name</th>
+				     	</tr>";
+					
+				while($row = mysqli_fetch_assoc($result)) {
+					echo "<tr><td>" . $row["selected"] . "</td><td>" . $row["category"] . "</td><td>" . $row["month"]
+				 	. "</td><td>" . $row["day"] . "</td><td>" . $row["name"] . "</td></tr>"; 
+				}
+				
+				echo "</table>";
 		
-		 echo "<tr><td>" . $row["selected"] . "</td><td>" . $row["category"] . "</td><td>" . $row["month"]
-		 . "</td><td>" . $row["day"] . "</td><td>" . $row["name"] . "</td></tr>";
-			 
-		 }
-		 
-		 echo "</table>";
-
-		 mysqli_free_result($result);
-	     mysqli_close($db);
-	    
-	     
-	     }
+				mysqli_free_result($result);
+			    mysqli_close($db);
+			    
+			     
+			     }
 	     ?>    
-	     
+	     </div>
 	
 </body>
 
